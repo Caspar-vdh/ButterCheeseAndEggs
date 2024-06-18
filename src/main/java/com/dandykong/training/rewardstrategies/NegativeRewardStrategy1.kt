584f6cfc6f4a93ccae6e8514d34ed0b747bed2dd
CASPARS-SURFACE\c_van <c_van_der_heiden@hotmail.com>
2024-06-19 00:57:34 +0200

    Negative reward strategy

--- src/main/java/com/dandykong/training/rewardstrategies/NegativeRewardStrategy1.kt
+++ src/main/java/com/dandykong/training/rewardstrategies/NegativeRewardStrategy1.kt
@@ -1,20 +1,19 @@
 package com.dandykong.training.rewardstrategies
 
-import com.dandykong.training.basics.MAX_WEIGHT
 import com.dandykong.training.basics.RewardStrategy
 import com.dandykong.training.basics.State
-import kotlin.math.min
+import kotlin.math.max
 
-class PositiveRewardStrategy1: RewardStrategy<State> {
+class NegativeRewardStrategy1: RewardStrategy<State> {
 
     /**
-     * Final state gets increased with 5 (or set to the max value, 255)
+     * Final state gets decreased with 5 (or set to the min value, 0)
      * Second last state (given there are two or more entries) with 4
      * etc... up to fifth last state (given there are 5 states) with 1
      */
     @OptIn(ExperimentalUnsignedTypes::class)
     override fun updateWeights(selectedActions: Map<State, Int>) {
-        var rewardValue = 5
+        var negativeRewardValue = 5
 
         for (entry in selectedActions.entries.reversed().iterator()) {
             val state = entry.key
@@ -22,9 +21,9 @@ class PositiveRewardStrategy1: RewardStrategy<State> {
 
             val weight = state.weights[action].toInt()
             // TODO: error handling in case there are no weights for state?
-            val newWeight = min(weight + rewardValue, MAX_WEIGHT.toInt())
+            val newWeight = max(weight - negativeRewardValue, 0)
             state.weights[action] = newWeight.toUByte()
-            rewardValue--
+            negativeRewardValue--
         }
     }
 }
